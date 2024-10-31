5e7a53215bfffb811958036d56b867dba0fe8bc4
ellie <91428237+elliegdev@users.noreply.github.com>
2024-10-31 11:40:12 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,2 @@
-// Função para mostrar o próximo enigma
-function showNextEnigma(id) {
-    document.querySelectorAll('.enigma-container').forEach(function(enigma) {
-        enigma.style.display = 'none';
-    });
-    document.getElementById(id).style.display = 'block';
-}
+
 
